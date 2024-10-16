5b165d6ee542dd6562567a290c19fed5dbf54ccc
VIPUL2433 <agvipul36@gmail.com>
2024-10-17 03:25:15 +0530

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,112 @@ class _MyHomePageState extends State<MyHomePage> {
               image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 85, left: 50, right: 34),
+              child: Text(
+                'Welcome User',
+                style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: //EdgeInsets.only(top: 200),
+                    EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.42,
+                  right: 35,
+                  left: 35,
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey.shade200,
+                        filled: true,
+                        hintText: 'Email',
+                        hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey.shade200,
+                        filled: true,
+                        hintText: 'password',
+                        hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Sign in',
+                          style: TextStyle(
+                            fontSize: 34,
+                            fontWeight: FontWeight.w900,
+                            color: Color(0xff4c505b),
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Color(0xff4c505b),
+                          child: IconButton(
+                            color: Colors.white,
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_forward),
+                            iconSize: 23,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 34,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Sign up',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Color(0xff4c505b),
+                                  fontWeight: FontWeight.w500,
+                                  decoration: TextDecoration.underline),
+                            )),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Forget password',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Color(0xff4c505b),
+                                  fontWeight: FontWeight.w500,
+                                  decoration: TextDecoration.underline),
+                            ))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
